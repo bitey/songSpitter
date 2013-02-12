81ac0a5b301107229516540370a28d2a40e34808
bitey <bitey@outlook.com>
2013-02-11 18:22:15 -0600

    Initial Commit

---
+++ songSpitter/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  songSpitter
+//
+//  Created by StopBitingMe on 2/11/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
